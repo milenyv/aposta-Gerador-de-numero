b00f37e3672d0f4b24f7639ddc424878fdc80f2f
mileny <milenyvelho@outlook.com>
2024-03-15 22:23:58 -0300

    Adicionar arquivos de projeto.

---
+++ Gerador de numero/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int quantidadeNumeros = 6;
+        int valorMinimo = 1;
+        int valorMaximo = 60;
+
+        List<int> aposta = GerarNumeroAposta(quantidadeNumeros, valorMinimo, valorMaximo);
+
+        Console.WriteLine("Números da aposta:");
+        foreach (int numero in aposta)
+        {
+            Console.Write(numero + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static List<int> GerarNumeroAposta(int qtdNumeros, int minValor, int maxValor)
+    {
+        List<int> numerosAposta = new List<int>();
+        Random random = new Random();
+
+        while (numerosAposta.Count < qtdNumeros)
+        {
+            int numero = random.Next(minValor, maxValor + 1);
+            if (!numerosAposta.Contains(numero))
+            {
+                numerosAposta.Add(numero);
+            }
+        }
+
+        numerosAposta.Sort();
+        return numerosAposta;
+    }
+}
